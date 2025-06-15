eee907e32fc4df86b2ce49cd3627199603a983c9
Anjali <k.anjaliii.1011@gmail.com>
2025-06-16 00:43:26 +0530

    minor changes

--- web-chat-frontend/src/components/SignIn.jsx
+++ web-chat-frontend/src/components/SignIn.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import  { useState } from 'react'
+import React, { useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { addUser } from '../utils/userSlice';
 import { useNavigate } from 'react-router-dom';
