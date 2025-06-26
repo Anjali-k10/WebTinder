70332519dbae17e315d22de2d7eb3f32cb589734
Anjali <k.anjaliii.1011@gmail.com>
2025-06-26 17:56:59 +0530

    adding backend deployed url

--- web-chat-frontend/src/utils/constants.js
+++ web-chat-frontend/src/utils/constants.js
@@ -1,2 +1,3 @@
-export const Base_URL ="http://localhost:7777"
+// export const Base_URL ="http://localhost:7777"
+export const Base_URL = "https://webtinder.onrender.com";
 export const defaultPhoto="https://static.vecteezy.com/system/resources/previews/008/442/086/original/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg"
